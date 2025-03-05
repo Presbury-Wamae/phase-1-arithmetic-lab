97650fc5779123d459a2c9c66d77119ec35f7d57
Presbury <wamaepresbury034@gmail.com>
2025-03-05 20:15:04 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+(multiply);
+
+
+const random = Math.floor(Math.random()*100) + 1;
+
+(random);
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+(mod)
+
+const max = Math.max(17, 18, 19, 20);
+
+(max)
+
